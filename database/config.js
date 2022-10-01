91a3fffb5d0b6d3c81dd81d39dcb34cb379a7c0c
Gustavo Bono <ggbono27@gmail.com>
2022-10-01 13:30:49 -0300

    .

--- database/config.js
+++ database/config.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-const CNN="mongodb+srv://ggbono:VfN791D8xGTPASz9@clustercafe.prnzd.mongodb.net/cafeDB"
-
 const dbConnection = async () => {
     try {
-        await mongoose.connect(CNN)
+        await mongoose.connect(process.env.MONGODB_CNN)
         console.log('DB SERVER connected')
     } catch (error) {
         console.log(error)
